5145afb5c6a05d42947074dea4b1c780e1d6fc9f
GopalDas123 <Gopaldax@gmail.com>
2020-06-21 12:50:10 +0500

    final

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import Gopal from './components/Gopal';
 
 import { GlobalProvider } from './context/GlobalState';
 
@@ -18,6 +19,7 @@ function App() {
         <IncomeExpenses />
         <TransactionList />
         <AddTransaction />
+        <Gopal />
       </div>
     </GlobalProvider>
   );
---
+++ src/components/Gopal.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Gopal = () => {
+    return <h5>Developed By <a href="https://github.com/GopalDas123" target="_blank">Gopal Das</a> </h5>
+}
+
+export default Gopal
